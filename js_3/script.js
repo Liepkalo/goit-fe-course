e5e09fa545d0fe3480c455831dc1ea442f302a4f
Oleg Liepkalo <liepkalo.o@gmail.com>
2018-12-26 22:15:44 +0200

    ch4

--- js_3/script.js
+++ js_3/script.js
@@ -1,42 +1,29 @@
 'use strict';
-
 let userLoginInput;
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 userLoginInput = prompt('Введите логин');
-let checkLoginUnique;
-let checkLoginValid;
-checkLoginUnique = logins.includes(userLoginInput);
+
 
 const isLoginUnique = function (logins, userLoginInput) {
-    return !checkLoginUnique;
+    return !logins.includes(userLoginInput);
 };
-
 
 const isLoginValid = function (userLoginInput) {
     if (userLoginInput.length >= 4 && userLoginInput.length <= 16) {
-        checkLoginValid = true;
-        return checkLoginValid;
+        return true;
     } else {
-        checkLoginValid = false;
-        return checkLoginValid;
+        return false;
     }
 };
 
-const addLogin = function (logins, login) {
-    if (checkLoginValid === true && checkLoginUnique === false) {
-        logins.push(userLoginInput);
-        console.log('логин успешно добавлен');
-        return logins;
-    } else if (checkLoginValid === false && checkLoginUnique === false) {
-        console.log("Ошибка! Логин должен быть от 4 до 16 символов");
-        return false;
-    } else {
-        console.log("Такой логин уже используется!");
-        return false;
-    }
-}
+const addLogin = function (logins, userLoginInput) {
+    if (userLoginInput === null) return "пользователь нажал отмену";
+    const valid = isLoginValid(userLoginInput);
+    if (!valid) return "Ошибка! Логин должен быть от 4 до 16 символов";
+    const unique = isLoginUnique(logins, userLoginInput);
+    if (!unique) return "Такой логин уже используется!";
+    logins.push(userLoginInput);
+    return console.log(logins, 'логин успешно добавлен');
+};
 
-
-console.log('Валидность:', isLoginValid(userLoginInput));
-console.log('Уникальность:', isLoginUnique(logins, userLoginInput));
 console.log(addLogin(logins, userLoginInput));
