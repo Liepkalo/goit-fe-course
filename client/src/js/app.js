df2ee1b41f7f8b6e84565b469c647caf2a8b5fc9
Oleg Liepkalo <liepkalo.o@gmail.com>
2019-04-10 02:06:21 +0300

    1

---
+++ client/src/js/app.js
@@ -0,0 +1,110 @@
+import Notyf from 'notyf';
+import Micromodal from 'micromodal';
+import initialNotes from '../assets/notes.json';
+import noteTemplate from '../templates/note.hbs';
+
+import {
+	NOTE_ACTIONS,
+	ICON_TYPES,
+	PRIORITY_TYPES
+} from './utils/constants.js';
+import Notepad from './utils/notepad-model';
+import saveNote from './utils/notepad-model';
+import {
+	getRefs,
+	renderListItems,
+	addItemToList
+} from './utils/view';
+
+import 'notyf/dist/notyf.min.css';
+
+const notepad = new Notepad();
+const refs = getRefs();
+const notyf = new Notyf();
+
+notepad
+	.get()
+	.then(serverNote =>
+		renderListItems(refs.list, notepad.notes)
+	);
+
+
+
+// Handlers
+const handleEditorSubmit = (event) => {
+	event.preventDefault();
+
+	const [input, input2] = event.currentTarget.elements;
+	const inputValue = input.value;
+	const inputValue2 = input2.value;
+
+	if (inputValue.trim() === '' || inputValue2.trim() === '') {
+		return notyf.alert('Необходимо заполнить все поля!');
+	}
+
+	const submitNote = {
+		title: inputValue,
+		body: inputValue2,
+		priority: Notepad.getPriorityName(PRIORITY_TYPES.LOW),
+	};
+
+	notepad
+		.saveNote(submitNote)
+		.then(saveItem => {
+			addItemToList(refs.list, saveNote);
+		})
+		.catch(Error => {
+			notyf.alert(Error);
+		});
+	notyf.confirm('Заметка успешно добавлена!');
+	Micromodal.close('note-editor-modal');
+
+	event.currentTarget.reset();
+
+};
+
+const handleFilterChange = (event) => {
+	//console.log(event.target.value);
+	const filteredItems = notepad.filter(event.target.value);
+	renderListItems(refs.list, filteredItems);
+};
+
+const removeListItem = (element) => {
+	const parentListItem = element.closest('.note-list__item');
+	const id = parentListItem.dataset.id;
+
+	notepad.deleteNote(id);
+	parentListItem.remove();
+	console.table(notepad._notes);
+};
+
+const handleListClick = ({
+	target
+}) => {
+	if (target.parentNode.nodeName !== 'BUTTON') return;
+
+	const action = target.parentNode.dataset.action;
+
+	switch (action) {
+		case NOTE_ACTIONS.DELETE:
+			console.log('delete');
+			removeListItem(target);
+
+			break;
+
+		default:
+			console.log('invalid action!');
+	}
+};
+
+const handleOpenEditor = () => {
+	Micromodal.show('note-editor-modal');
+};
+
+renderListItems(refs.list, notepad.notes);
+
+// Listeners
+refs.editor.addEventListener('submit', handleEditorSubmit);
+refs.filter.addEventListener('input', handleFilterChange);
+refs.list.addEventListener('click', handleListClick);
+refs.openEditorModalBtn.addEventListener('click', handleOpenEditor);
---
+++ client/src/js/utils/notepad-model.js
@@ -0,0 +1,76 @@
+import initialNotes from '../../assets/notes.json';
+import * as api from '../../services/api';
+const shortid = require('shortid');
+import {
+    PRIORITY_TYPES,
+    PRIORITIES
+} from './constants.js';
+
+
+export default class Notepad {
+
+    constructor(notes = []) {
+        this._notes = notes;
+    }
+
+    get notes() {
+        return this._notes;
+    }
+
+    get() {
+        return api.getNotes().then(notes => {
+            this._notes = notes;
+
+            return this._notes;
+        });
+    }
+
+    saveNote(note) {
+        return api.saveNotes(note).then(newNote => {
+            this._notes.push(newNote);
+
+            return newNote;
+        });
+    }
+
+    deleteNote(id) {
+        return api.deleteNoteId(id).then(() => {
+            this._notes = this._notes.filter(note => note.id !== id);
+
+            return id;
+        });
+    }
+
+
+    filter(query = '') {
+        return this._notes.filter(
+            (note) =>
+            note.body.toLowerCase().includes(query.toLowerCase()) ||
+            note.title.toLowerCase().includes(query.toLowerCase())
+        );
+    }
+
+
+    static getPriorityName(priorityId) {
+        const priorityValues = Object.values(this.PRIORITIES);
+        for (const value of priorityValues) {
+            if (value.id === priorityId) {
+                return value.name;
+            }
+        }
+    }
+
+
+
+    filterNotesByPriority(priority) {
+        const filteredByPriorityNotes = [];
+        for (const note of this._notes) {
+            if (note.priority === priority) {
+                filteredByPriorityNotes.push(note);
+                return filteredByPriorityNotes;
+            }
+        }
+    }
+
+}
+Notepad.PRIORITIES = PRIORITIES;
