e63df3ba77a9457b9e401d62da138aab72c439b0
Oleg Liepkalo <liepkalo.o@gmail.com>
2019-05-26 17:21:34 +0300

    Updates

---
+++ precache-manifest.83204588f50c31aefc35f68464b0a181.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d7c3724fae867f94c917f47a87d56e",
+    "url": "/goit-fe-course/index.html"
+  },
+  {
+    "revision": "091ed3de02a2cfcc71f9",
+    "url": "/goit-fe-course/static/css/main.194dd4d6.chunk.css"
+  },
+  {
+    "revision": "c9563366c89b046a3ab0",
+    "url": "/goit-fe-course/static/js/2.b100127d.chunk.js"
+  },
+  {
+    "revision": "091ed3de02a2cfcc71f9",
+    "url": "/goit-fe-course/static/js/main.b6ee9382.chunk.js"
+  },
+  {
+    "revision": "6fc20f7e957d07e7940e",
+    "url": "/goit-fe-course/static/js/runtime~main.e6ce5aa7.js"
+  }
+]);
